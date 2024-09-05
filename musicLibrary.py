3ba404070cf8ad89e8fbc401dfbae73b75eb4570
Sikum Hangma Madi <127391841+siikum@users.noreply.github.com>
2024-09-05 12:45:31 +0545

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,7 @@
+music = {
+    "game": "https://www.youtube.com/watch?v=TZE9gVF1QbA&pp=ygUJZ290IHRoZW1l",
+    "goodbye": "https://www.youtube.com/watch?v=1RKqOmSkGgM&pp=ygUOZ29vZCBsdWNrIGJhYmU%3D",
+    "tetris": "https://www.youtube.com/watch?v=NmCCQxVBfyM&pp=ygUMdGV0cmlzIHRoZW1l",
+    "hold on tight": "https://i.ytimg.com/an_webp/1XZRhrt1Tkg/mqdefault_6s.webp?du=3000&sqp=CJD7orUG&rs=AOn4CLB_E8ZPqe23kb6Dew7j1PAyIQMuyA"
+    
+}
